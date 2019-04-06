a24f2e822b93f9fe495a66d11872cdc5a9cf4cef
Jawad Ahmed <jawadahmedtaj@gmail.com>
2019-04-06 14:19:19 +0200

    Layout set, Function call on click added

--- app/src/main/java/com/example/misexc01/MainActivity.java
+++ app/src/main/java/com/example/misexc01/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.misexc01;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView urlText;
+    EditText outputText;
+
+    public void buttonClick(View view){
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        urlText = findViewById(R.id.urlTextView);
+        outputText = findViewById(R.id.outputText);
     }
 }
