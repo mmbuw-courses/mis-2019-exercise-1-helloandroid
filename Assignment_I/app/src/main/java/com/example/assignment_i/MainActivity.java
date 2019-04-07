67af6798ad60b15f51113470ca5848ca1e12a80a
ChrisHeiden <christopherheiden9@gmail.com>
2019-04-07 15:02:07 +0200

    add APK file

--- Assignment_I/app/src/main/java/com/example/assignment_i/MainActivity.java
+++ Assignment_I/app/src/main/java/com/example/assignment_i/MainActivity.java
@@ -6,42 +6,115 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.NetworkError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
+   // Source: https://www.youtube.com/watch?v=dFlPARW5IX8
+   private Button search;
+   private TextView textView;
+   private EditText url;
 
-   Button search;
-   TextView textView;
-   EditText url;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
 
-      textView = (TextView) findViewById(R.id.textView);
-      url = (EditText) findViewById(R.id.url);
-      search = (Button) findViewById(R.id.search);
+      textView = findViewById(R.id.textView);
+      url = findViewById(R.id.url);
+      //Source: https://www.youtube.com/watch?v=hOlx11qQSq8
+      search = findViewById(R.id.search);
+   }
 
-      //final String server_url = "https:/www.google.com";
-      //final String server_url = "https://api.ipify.org/?format=json";
+   public void searchURL(View v){
+      String server_url = url.getText().toString();
+      server_url = redefineURL(server_url);
 
-      //url.setText(server_url);
+      //Source: https://developer.android.com/training/volley/simple
+      final RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+      StringRequest stringRequest = new StringRequest(Request.Method.GET, server_url, getResonse, getError);
+     requestQueue.add(stringRequest);
+     url.setText("");
+   }
 
+   //Source: https://stackoverflow.com/questions/25994514/volley-timeout-error
+   Response.ErrorListener getError = new Response.ErrorListener() {
+      @Override
+      public void onErrorResponse(VolleyError error) {
+         if (error instanceof NetworkError) {
+            Toast.makeText(MainActivity.this,
+                 "No internet connection",
+                 Toast.LENGTH_LONG).show();
+         } else if (error instanceof ServerError) {
+            Toast.makeText(MainActivity.this,
+                 "Server not found",
+                 Toast.LENGTH_LONG).show();
+         } else if (error instanceof AuthFailureError) {
+            Toast.makeText(MainActivity.this,
+                 "authentication is missing",
+                 Toast.LENGTH_LONG).show();
+         } else if (error instanceof TimeoutError) {
+            Toast.makeText(MainActivity.this,
+                 "Oops. Timeout error!",
+                 Toast.LENGTH_LONG).show();
+         }
+      }
+   };
+
+   Response.Listener<String> getResonse = new Response.Listener<String>() {
+      @Override
+      public void onResponse(String response) {
+         textView.setText(response);
+      }
+   };
+
+   private String redefineURL(String url) {
+      String newString = "";
+      if(url != "https")
+      {
+         int index = url.indexOf("http");
+         int i = 0;
+         boolean addS = true;
+         while(i<url.length()){
+            if(i == (index + 4) && addS) {
+               newString += 's';
+               i = 4;
+               addS = false;
+            }
+            else {
+               newString += url.charAt(i);
+               ++i;
+            }
+         }
+      }
+      return newString;
+   }
+}
+
+
+
+/*
       search.setOnClickListener(new View.OnClickListener() {
          @Override
          public void onClick(View v) {
             String server_url = url.getText().toString();
             server_url = redefineURL(server_url);
-            textView.setText(server_url);
+            //textView.setText(server_url);
 
+            //Source: https://developer.android.com/training/volley/simple
             final RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+            StringRequest stringRequest = new StringRequest(Request.Method.GET, server_url, getResonse, getError);
 
 
             StringRequest stringRequest = new StringRequest(Request.Method.GET, server_url,
@@ -63,28 +136,4 @@ public class MainActivity extends AppCompatActivity {
             requestQueue.add(stringRequest);
             url.setText("");
          }
-      });
-   }
-
-   private String redefineURL(String url) {
-      String newString = "";
-      if(url != "https")
-      {
-         int index = url.indexOf("http");
-         int i = 0;
-         boolean addS = true;
-         while(i<url.length()){
-            if(i == (index + 4) && addS == true) {
-               newString += 's';
-               i = 4;
-               addS = false;
-            }
-            else {
-               newString += url.charAt(i);
-               ++i;
-            }
-         }
-      }
-      return newString;
-   }
-}
+      });*/
