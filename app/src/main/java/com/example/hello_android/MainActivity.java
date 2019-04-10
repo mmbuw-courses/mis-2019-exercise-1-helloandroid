27f1c98bfd4d988859027ad1d574861eb6d923ef
MaximilianMargraf <m.margraf.m@googlemail.com>
2019-04-10 18:19:44 +0200

    WebView added. Seems to have certain problems with the emulator, but works fine on phones.

--- app/src/main/java/com/example/hello_android/MainActivity.java
+++ app/src/main/java/com/example/hello_android/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,12 +23,16 @@ import com.android.volley.toolbox.HurlStack;
 import com.android.volley.toolbox.StringRequest;
 import android.text.method.ScrollingMovementMethod;
 
+// for Volley I used this "https://developer.android.com/training/volley/requestqueue"
+// for general questions "https://developer.android.com/training/basics/firstapp" was used.
+
 public class MainActivity extends AppCompatActivity {
 
     EditText uInput;
     TextView uOutput;
     RequestQueue requestQueue;
     Toast toast;
+    WebView uWebsite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,17 +55,25 @@ public class MainActivity extends AppCompatActivity {
         requestQueue.start();
 
         uInput = (EditText) findViewById(R.id.editText);
-        String url = uInput.getText().toString();
+        final String url = uInput.getText().toString();
 
         // Formulate the request and handle the response.
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                        // display text in the field under the input
+                        // display the html source code in the field under the input
                         uOutput = findViewById(R.id.textView);
                         uOutput.setText(response);
                         uOutput.setMovementMethod(new ScrollingMovementMethod());
+
+                        // display the website under the source code properly
+                        uWebsite = (WebView) findViewById(R.id.webView);
+                        // open website in App
+                        uWebsite.setWebViewClient(new WebViewClient());
+                        uWebsite.loadUrl(url);
+                        // it seems WebView has problems with the emulator; however,
+                        // it worked totally fine when I tried it with a phone.
                     }
                 },
                 new Response.ErrorListener() {
