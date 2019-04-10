7f92161ce46ce51b59fe41f8369528400a177321
sharadbrat <sharoobrazno@mail.ru>
2019-04-10 19:17:14 +0200

    added implementation of the exercise

--- app/src/main/java/com/nika_georgii/assignment1/MainActivity.java
+++ app/src/main/java/com/nika_georgii/assignment1/MainActivity.java
@@ -1,11 +1,14 @@
 package com.nika_georgii.assignment1;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -19,6 +22,9 @@ public class MainActivity extends AppCompatActivity {
     private TextView textView;
     private Button connectButton;
     private EditText input;
+    private WebView webView;
+
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +33,9 @@ public class MainActivity extends AppCompatActivity {
 
         connectButton = findViewById(R.id.button);
         textView = findViewById(R.id.textView);
+        webView = findViewById(R.id.webView);
         input = findViewById(R.id.editText);
+        progressBar = findViewById(R.id.progressBar);
 
         connectButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,23 +43,55 @@ public class MainActivity extends AppCompatActivity {
                 onConnectButtonClick(input.getText().toString());
             }
         });
+
+        resetViews();
+        setVisibilityOfProgressBar(false);
+    }
+
+    private void resetViews() {
+        webView.setVisibility(View.INVISIBLE);
+        textView.setVisibility(View.INVISIBLE);
+    }
+
+    private void setVisibilityOfProgressBar(boolean isVisible) {
+        if (isVisible) {
+            progressBar.setVisibility(View.VISIBLE);
+        } else {
+            progressBar.setVisibility(View.INVISIBLE);
+        }
     }
 
     private void onConnectButtonClick(String url) {
         RequestQueue queue = Volley.newRequestQueue(this);
+        resetViews();
 
         StringRequest stringRequest = new StringRequest(Request.Method.GET, url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
-                // Display the first 500 characters of the response string.
-                textView.setText("Response is: " + response.substring(0, 500));
+                setVisibilityOfProgressBar(false);
+                if (response.substring(0, 100).toLowerCase().startsWith("<!doctype html>")) {
+                    webView.setVisibility(View.VISIBLE);
+                    webView.loadDataWithBaseURL("", response, "text/html", "UTF-8", "");
+                } else {
+                    textView.setText(response.substring(0, 500));
+                    textView.setVisibility(View.VISIBLE);
+                }
             }
 
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
-                textView.setText("That didn't work!");
+                setVisibilityOfProgressBar(false);
+                Toast.makeText(getApplicationContext(), getPrettyErrorText(error), Toast.LENGTH_LONG).show();
             }
         });
+
+        queue.add(stringRequest);
+
+        setVisibilityOfProgressBar(true);
+    }
+
+    private String getPrettyErrorText(VolleyError error) {
+        return "Oooops, something went wrong. Error: " + error.getMessage();
     }
 }
