a3df6e5b5799e77fedbd5acf7d2c4f69dc407fdd
Jyotsna Gorle <jyotsnagorle@gmail.com>
2019-04-11 17:12:18 +0200

    clean up the view a little

--- Assignment_I/app/src/main/java/com/example/assignment_i/MainActivity.java
+++ Assignment_I/app/src/main/java/com/example/assignment_i/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.assignment_i;
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -29,160 +34,167 @@ import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
-   // Source: https://www.youtube.com/watch?v=dFlPARW5IX8
-   private Button search;
-   private Button searchType;
 
-   private TextView textView;
-   private EditText url;
-   private ImageView imageView;
-   private boolean json;
-   private boolean plainText;
-
+    private TextView resultTextview;
+    private EditText url;
+    private ImageView searchImageResult;
+    private boolean plainText;
+    private SwitchCompat isJsonResponse;
+    private Button connectButton;
+    private Button showImageButton;
 
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-   @Override
-   protected void onCreate(Bundle savedInstanceState) {
-      super.onCreate(savedInstanceState);
-      setContentView(R.layout.activity_main);
+        //Source: https://www.youtube.com/watch?v=hOlx11qQSq8
+        resultTextview = findViewById(R.id.resultTextView);
+        connectButton = findViewById(R.id.search);
+        // Source: https://www.youtube.com/watch?v=dFlPARW5IX8
+        showImageButton = findViewById(R.id.ShowImage);
+        searchImageResult = findViewById(R.id.resultimageView);
+        isJsonResponse = findViewById(R.id.toggle_response_type);
+        url = findViewById(R.id.url);
 
-      //Source: https://www.youtube.com/watch?v=hOlx11qQSq8
-      textView = findViewById(R.id.textView);
-      url = findViewById(R.id.url);
-      search = findViewById(R.id.search);
-      searchType = findViewById(R.id.type);
-      imageView = findViewById(R.id.imageView);
-      json = false;
-      plainText = true;
-   }
+        url.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
 
-   public void showImage(View v){
-      String imageURL = "http://ageofwonders.com/wp-content/uploads/2014/02/joker-face-painting.jpg";
-      Picasso.get().load(imageURL).into(imageView);
-   }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) { }
 
-
-   public void searchType(View v){
-      //Source: https://stackoverflow.com/questions/11154898/android-programmatically-setting-button-texts
-      if(plainText == true)
-      {
-         json = true;
-         plainText  = false;
-         searchType.setText("Json");
-      }
-      else
-      {
-         json = false;
-         plainText  = true;
-         searchType.setText("Plain Text");
-      }
-   }
-
-   public void searchURL(View v){
-      String server_url = url.getText().toString();
-      server_url = redefineURL(server_url);
-      if(plainText == true){
-         //Source: https://developer.android.com/training/volley/simple
-         final RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
-         StringRequest stringRequest =
-              new StringRequest(Request.Method.GET,
-                                server_url,
-                                getPlainTextResponse,
-                                getError);
-         requestQueue.add(stringRequest);
-      }
-      else{
-         Context mContext;
-         mContext = getApplicationContext();
-
-         RequestQueue requestQueue = Volley.newRequestQueue(mContext);
-         JsonObjectRequest jsonObjectRequest =
-              new JsonObjectRequest(Request.Method.GET,
-                                    server_url,
-                                   null,
-                                    getJsonResponse,
-                                    getError);
-         requestQueue.add(jsonObjectRequest);
-      }
-      url.setText("");
-   }
-
-   Response.Listener<JSONObject> getJsonResponse = new Response.Listener<JSONObject>(){
-      @Override
-      public void onResponse(JSONObject response) {
-         try{
-            JSONArray array = response.getJSONArray("students");
-            for(int i=0;i<array.length();i++){
-               JSONObject student = array.getJSONObject(i);
-               String firstName = student.getString("firstname");
-               String lastName = student.getString("lastname");
-               String age = student.getString("age");
-               textView.append(firstName +" " + lastName +"\nage : " + age);
-               textView.append("\n\n");
+            @Override
+            public void afterTextChanged(Editable s) {
+                String currentText = s.toString();
+                if (currentText.endsWith(".jpg") || currentText.endsWith(".jpeg") || currentText.endsWith(".png") || currentText.endsWith(".bmp")) {
+                    showImageButton.setEnabled(true);
+                    connectButton.setEnabled(false);
+                } else {
+                    showImageButton.setEnabled(false);
+                    connectButton.setEnabled(true);
+                }
             }
-         }catch (JSONException e){
-            e.printStackTrace();
-         }
-      }
-   };
+        });
 
-   //Source: https://stackoverflow.com/questions/25994514/volley-timeout-error
-   Response.ErrorListener getError = new Response.ErrorListener() {
-      @Override
-      public void onErrorResponse(VolleyError error) {
-         if (error instanceof NetworkError) {
-            Toast.makeText(MainActivity.this,
-                 "No internet connection",
-                 Toast.LENGTH_LONG).show();
-         } else if (error instanceof ServerError) {
-            Toast.makeText(MainActivity.this,
-                 "Server not found",
-                 Toast.LENGTH_LONG).show();
-         } else if (error instanceof AuthFailureError) {
-            Toast.makeText(MainActivity.this,
-                 "authentication is missing",
-                 Toast.LENGTH_LONG).show();
-         } else if (error instanceof TimeoutError) {
-            Toast.makeText(MainActivity.this,
-                 "Oops. Timeout error!",
-                 Toast.LENGTH_LONG).show();
-         }
-      }
-   };
-
-   Response.Listener<String> getPlainTextResponse = new Response.Listener<String>() {
-      @Override
-      public void onResponse(String response) {
-         textView.setText(response);
-      }
-   };
-
-   private String redefineURL(String url) {
-      String newString = "";
-      if(url.charAt(0) == 'w')
-      {
-         newString = "https://" + url;
-      }
-      else if(url.substring(0,5) != "https")
-      {
-         int index = url.indexOf("http");
-         int i = 0;
-         boolean addS = true;
-         while(i<url.length()){
-            if(i == (index + 4) && addS) {
-               newString += 's';
-               i = 4;
-               addS = false;
+        plainText = true;
+        isJsonResponse.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) plainText = false;
+                else plainText = true;
             }
-            else {
-               newString += url.charAt(i);
-               ++i;
+        });
+    }
+
+    public void showImage(View v) {
+        String imageURL = "http://ageofwonders.com/wp-content/uploads/2014/02/joker-face-painting.jpg";
+//        String imageURL = String.valueOf(url.getText());
+        Picasso.get().load(imageURL).into(searchImageResult);
+    }
+
+    public void searchURL(View v) {
+        String server_url = url.getText().toString();
+        resultTextview.setText("");
+        server_url = redefineURL(server_url);
+        if (plainText) {
+            //Source: https://developer.android.com/training/volley/simple
+            final RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
+            StringRequest stringRequest =
+                    new StringRequest(Request.Method.GET,
+                            server_url,
+                            getPlainTextResponse,
+                            getError);
+            requestQueue.add(stringRequest);
+        } else {
+            Context mContext;
+            mContext = getApplicationContext();
+
+            RequestQueue requestQueue = Volley.newRequestQueue(mContext);
+            JsonObjectRequest jsonObjectRequest =
+                    new JsonObjectRequest(Request.Method.GET,
+                            server_url,
+                            null,
+                            getJsonResponse,
+                            getError);
+            requestQueue.add(jsonObjectRequest);
+        }
+        url.setText("");
+    }
+
+    //Source: https://android--examples.blogspot.com/2017/02/android-volley-json-object-request.html
+    Response.Listener<JSONObject> getJsonResponse = new Response.Listener<JSONObject>() {
+        @Override
+        public void onResponse(JSONObject response) {
+            try {
+                JSONArray array = response.getJSONArray("students");
+                for (int i = 0; i < array.length(); i++) {
+                    JSONObject student = array.getJSONObject(i);
+                    String firstName = student.getString("firstname");
+                    String lastName = student.getString("lastname");
+                    String age = student.getString("age");
+                    resultTextview.append(firstName + " " + lastName + "\nage : " + age);
+                    resultTextview.append("\n\n");
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
             }
-         }
-      }
-      return newString;
-   }
+        }
+    };
+
+    //Source: https://stackoverflow.com/questions/25994514/volley-timeout-error
+    Response.ErrorListener getError = new Response.ErrorListener() {
+        @Override
+        public void onErrorResponse(VolleyError error) {
+            if (error instanceof NetworkError) {
+                Toast.makeText(MainActivity.this,
+                        "No internet connection",
+                        Toast.LENGTH_LONG).show();
+            } else if (error instanceof ServerError) {
+                Toast.makeText(MainActivity.this,
+                        "Server not found",
+                        Toast.LENGTH_LONG).show();
+            } else if (error instanceof AuthFailureError) {
+                Toast.makeText(MainActivity.this,
+                        "authentication is missing",
+                        Toast.LENGTH_LONG).show();
+            } else if (error instanceof TimeoutError) {
+                Toast.makeText(MainActivity.this,
+                        "Oops. Timeout error!",
+                        Toast.LENGTH_LONG).show();
+            }
+        }
+    };
+
+    Response.Listener<String> getPlainTextResponse = new Response.Listener<String>() {
+        @Override
+        public void onResponse(String response) {
+            resultTextview.setText(response);
+        }
+    };
+
+    private String redefineURL(String url) {
+        String newString = "";
+        if (url.charAt(0) == 'w') {
+            newString = "https://" + url;
+        } else if (url.substring(0, 5) != "https") {
+            int index = url.indexOf("http");
+            int i = 0;
+            boolean addS = true;
+            while (i < url.length()) {
+                if (i == (index + 4) && addS) {
+                    newString += 's';
+                    i = 4;
+                    addS = false;
+                } else {
+                    newString += url.charAt(i);
+                    ++i;
+                }
+            }
+        }
+        return newString;
+    }
 }
 
 
@@ -193,7 +205,7 @@ public class MainActivity extends AppCompatActivity {
          public void onClick(View v) {
             String server_url = url.getText().toString();
             server_url = redefineURL(server_url);
-            //textView.setText(server_url);
+            //resultTextview.setText(server_url);
 
             //Source: https://developer.android.com/training/volley/simple
             final RequestQueue requestQueue = Volley.newRequestQueue(MainActivity.this);
@@ -204,14 +216,14 @@ public class MainActivity extends AppCompatActivity {
                  new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-                       textView.setText(response);
+                       resultTextview.setText(response);
                        requestQueue.stop();
                     }
                  },
                  new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                       textView.setText(": Something goes wrong");
+                       resultTextview.setText(": Something goes wrong");
                        error.printStackTrace();
                        requestQueue.stop();
                     }
