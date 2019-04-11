4548f5afb49ef77cf57825c6b1c7bdae03370377
julienbreunig <julien.breunig@uni-weimar.de>
2019-04-11 19:26:29 +0200

    add volley, delete floating button

--- exercise_1/app/src/main/java/com/example/exercise_1/MainActivity.java
+++ exercise_1/app/src/main/java/com/example/exercise_1/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.exercise_1;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.toolbox.Volley;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText urlEditText;
+    private Button connectButton;
+    private TextView textView;
+
+    private String urlString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,35 +30,42 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        urlEditText = findViewById(R.id.edittext_url);
+        connectButton = findViewById(R.id.button_connect);
+        textView = findViewById(R.id.textview_output);
+
+        connectButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                onConnect(urlEditText);
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+    protected void onConnect(EditText urlEditText) {
+        urlString = urlEditText.getText().toString();
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, urlString,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        textView.setText("Response is: "+ response.substring(0,500));
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        textView.setText("That didn't work!");
+                    }
+                }
+        );
 
-        return super.onOptionsItemSelected(item);
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
     }
 }
