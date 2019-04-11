e958d8ed83c157a41aacfd1c164faab7c794ac8c
yorrickslr <yoyo.sieler@gmail.com>
2019-04-12 00:55:11 +0200

    Finish error handling

--- app/src/main/java/com/awesometek/mis1/MainActivity.java
+++ app/src/main/java/com/awesometek/mis1/MainActivity.java
@@ -12,10 +12,17 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.text.method.ScrollingMovementMethod;
 import android.widget.Toast;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import com.android.volley.*;
 import com.android.volley.toolbox.*;
 
+import java.net.UnknownHostException;
+
 public class MainActivity extends AppCompatActivity {
+
+    // html rendering adapted from https://youtu.be/cfk5f0R6BmE
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,9 +32,9 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         final EditText urlText = findViewById(R.id.urlText);
-        urlText.setText("https://awesometek.com");
         final TextView outputText = findViewById(R.id.outputText);
         outputText.setMovementMethod(new ScrollingMovementMethod());
+        webView = findViewById(R.id.outputHTML);
         final Button connectButton = findViewById(R.id.connect);
         connectButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,13 +52,34 @@ public class MainActivity extends AppCompatActivity {
                         new Response.Listener<String>() {
                             @Override
                             public void onResponse(String response) {
-                                // Display the first 500 characters of the response string.
-                                outputText.setText("Response is: " + response.substring(0, 500));
+                                outputText.setText(response.substring(0, 1000));
+                                webView.setWebViewClient(new WebViewClient());
+                                webView.loadData(response, "text/html", "UTF-8");
                             }
                         }, new Response.ErrorListener() {
                             @Override
                             public void onErrorResponse(VolleyError error) {
-                                Toast.makeText(getApplicationContext(), "ERROR: " + error.getMessage(), Toast.LENGTH_LONG).show();
+                                String errorMessage = error.getMessage();
+                                // error handling adapted from https://stackoverflow.com/a/31476034
+                                if(error.networkResponse != null) {
+                                    if(error.networkResponse.statusCode == 404) {
+                                        errorMessage = "File not found";
+                                    } else if(error.networkResponse.statusCode == 408) {
+                                        errorMessage = "Request Timeout";
+                                    }
+                                }
+                                if(error instanceof TimeoutError) {
+                                    errorMessage = "Connection Timeout";
+                                } else if(error instanceof NoConnectionError ||
+                                        error.getCause() instanceof UnknownHostException) {
+                                    errorMessage = "No Connection";
+                                } else if(error instanceof NetworkError) {
+                                    errorMessage = "Network Error";
+                                } else {
+                                    errorMessage = "Other";
+                                }
+                                Toast.makeText(getApplicationContext(),
+                                        "ERROR: " + errorMessage, Toast.LENGTH_LONG).show();
                             }
                     });
                     requestQueue.add(stringRequest);
@@ -63,12 +91,14 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onDestroy() {
         super.onDestroy();
+        if(webView != null) {
+            webView.destroy();
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        Toast.makeText(getApplicationContext(), "Bye!", Toast.LENGTH_LONG).show();
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
