db4b33d743dcffeda1f2593dd2c411e79e8bfd25
Thang Nguyen <nhthang1009@gmail.com>
2019-04-11 22:23:09 +0200

    First Commit Code

---
+++ app/src/main/java/com/example/exercise_1/MainActivity.java
@@ -0,0 +1,117 @@
+package com.example.exercise_1;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    //Additional code
+    String url_str = new String();
+    Button submit_button;
+    EditText input_text;
+    WebView web_view;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Additional Code
+        submit_button = findViewById(R.id.button);
+        input_text = findViewById(R.id.editText);
+        web_view = findViewById(R.id.web_view);
+        WebSettings setup = web_view.getSettings();
+        setup.setUseWideViewPort(true);
+        setup.setLoadWithOverviewMode(true);
+
+        web_view.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url){
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+            }
+        });
+
+        submit_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InputMethodManager imm =
+                        (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(input_text.getWindowToken(), 0);
+
+                url_str = input_text.getText().toString();
+                if(url_str.length() == 0){
+                    showToast("Please input an URL here");
+                }else {
+                    web_view.loadUrl("about:blank");
+                    if (!url_str.startsWith("http://") && !url_str.startsWith("https://")) { //from add_http
+                        url_str = "https://" + url_str;
+                    }
+                    web_view.loadUrl(url_str);
+                    web_view.requestFocus();
+
+                }
+            }
+        });
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private void loadWebView() {
+        web_view.loadUrl(url_str); //from display_url
+        web_view.requestFocus();
+    }
+
+    private void showToast(String msg) {
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        toast.show();
+    }
+
+}
